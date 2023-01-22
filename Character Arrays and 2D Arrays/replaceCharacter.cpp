be8257353a5e82e353daa1adc40357b44063a6df
Codemigod-666 <vikay434@gmail.com>
2023-01-22 14:27:53 +0530

    replace character solution commit

---
+++ Character Arrays and 2D Arrays/replaceCharacter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+void replaceCharacter(char input[], char c1, char c2){
+    for(int i = 0 ; i< strlen(input) ; i++){
+        if(input[i] == c1){
+            input[i] = c2;
+        }
+    }
+}
+// #include "solution.h"
+// THIS IS THE ALREADY  GIVEN MAIN FUNCTION
+int main() {
+    char input[1000000];
+    char c1, c2;
+    cin >> input;
+    cin >> c1 >> c2;
+    replaceCharacter(input, c1, c2);
+    cout << input << endl;
+}
