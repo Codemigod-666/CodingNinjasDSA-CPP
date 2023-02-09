3c6393765c4332e2c7246eba4bc553186bcb5088
Codemigod-666 <vikay434@gmail.com>
2023-02-09 16:32:49 +0530

    changes commit

--- Arrays/arraysum.cpp
+++ Arrays/arraysum.cpp
@@ -5,13 +5,13 @@ int main(){
     /* Read input as specified in the question.
      * Print output as specified in the question.
      */
-     int N;
-     cin>>N;
+     int n;
+     cin>>n;
 
-     int * arr= new int (N);
+     int * arr= new int (n);
      int sum = 0;
 
-     for(int i = 0; i< N ; i++){
+     for(int i = 0; i< n ; i++){
          cin>>arr[i];
          sum+= arr[i];
      }
